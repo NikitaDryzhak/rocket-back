899fc39d13ee3a55da48a3a6e647ce2a63d7999e
Nikita Dryzhak <87204570+NikitaDryzhak@users.noreply.github.com>
2022-10-07 18:23:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/players", contactsRouter);
+app.use("/", contactsRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
